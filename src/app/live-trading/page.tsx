f10b016f408da0b852d070ed48fa3475c7cf1f23
Aaryaman Jaiswal <jaiswalaaryaman@gmail.com>
2025-11-26 08:11:46 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/live-trading/page.tsx
+++ src/app/live-trading/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, 'react';
+import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import LiveControlBar from '@/components/live/LiveControlBar';
